f7fa434c3b7a9491be2a59adfc41c91952ba30b2
davidizzy <davidizroy@gmail.com>
2017-03-20 17:30:56 -0500

    Commit for Flicklist 1 Studio

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
-        # TODO: make a list with at least 5 movie titles
+        # make a list with at least 5 movie titles
+        movies = [
+            ("The Big Lebowski"),
+            ("Secret Life of Pets"),
+            ("Moana"),
+            ("Rogue One: A Star Wars Story"),
+            ("Doctor Strange")
+        ]
 
-        # TODO: randomly choose one of the movies, and return it
+        # randomly choose one of the movies, and return it
+        randIndex = random.randint(0, (len(movies)-1))
 
-        return "The Big Lebowski"
+        return movies[randIndex]
 
     def get(self):
         # choose a movie by invoking our new function
-        movie = self.getRandomMovie()
+        movieToday = self.getRandomMovie()
+        movieTomorrow = self.getRandomMovie()
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
-        content += "<p>" + movie + "</p>"
+        content += "<p>" + movieToday + "</p>"
 
-        # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
+        # pick a different random movie, and display it under
+        content += "<h1>Tommorrow's Movie</h1>"
+        content += "<p>" + movieTomorrow + "</p>"
 
         self.response.write(content)
 
